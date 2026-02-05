40d2749d81108b059944314b1d5a210e3d7f5d2f
Krishna Som <your-email@gmail.com>
2026-02-05 15:52:46 +0530

    Add pattern printing implementations for various row configurations

---
+++ COLLEGE/Loops/Pattern_printing/Q2.py
@@ -0,0 +1,9 @@
+n=int(input("Enter the number of rows: "))
+for i in range(1,n+1):
+    for j in range(n-i,0,-1):
+        print(" ",end="")
+    
+    for j in range(1,2*i):
+        print("*",end="")
+    
+    print()
