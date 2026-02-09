9b71839051875007d8fe0c805803dc08573a5f89
Krishna Som <your-email@gmail.com>
2026-02-09 20:58:19 +0530

    Updated

--- COLLEGE/Experiment 3/Q3.py
+++ COLLEGE/Experiment 3/Q3.py
@@ -1,11 +1,15 @@
-def fibbonaci(n):
-    if n<=0:
-        return 0
-    elif n==1:
-        return 1
-    else:
-        return fibbonaci(n-1)+fibbonaci(n-2)
-    
+#fibbonaci series
 n=int(input("Enter the number of terms: "))
-for i in range(n):
-    print(fibbonaci(i),end=" ")
+a, b = 0, 1
+if n <= 0:
+    print("Please enter a positive integer.")
+elif n == 1:
+    print("Fibonacci sequence up to", n, ":")
+    print(a)
+else:
+    print("Fibonacci sequence up to", n, ":")
+    print(a, end=' ')
+    print(b, end=' ')
+    for _ in range(2, n):
+        a, b = b, a + b
+        print(b, end=' ')
--- COLLEGE/Q1.py
+++
@@ -1,8 +0,0 @@
-a=input("Enter the character : ")
-
-if a.isalpha():
-    print("The entered character is a alphabet")  
-elif(a.isdigit()):
-    print("The entered character is a digit")
-else:
-    print("The entered character is a special character")
