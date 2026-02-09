0f5278213889def8b07144ae725b5ea93c4ac7d0
Krishna Som <your-email@gmail.com>
2026-02-09 21:04:22 +0530

    Refactor palindrome check implementation for clarity and efficiency

--- COLLEGE/Experiment 3/Q5.py
+++ COLLEGE/Experiment 3/Q5.py
@@ -1,12 +1,12 @@
-def rev(x):
-    rev=0
-    while(x>0):
-        rev=rev*10+x%10
-        x//=10
-    return rev
-x=int(input("Enter the number : "))
-y=rev(x)
-if(x==y):
+x = int(input("Enter the number: "))
+temp = x
+y = 0
+
+for i in range(len(str(x))):
+    y = (y * 10) + (x % 10)
+    x //= 10
+
+if temp == y:
     print("Palindrome Number")
 else:
     print("Not a Palindrome Number")
