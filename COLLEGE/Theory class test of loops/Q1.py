2381e68960289e7316c73eb3de3f370a61402ed3
Krishna Som <your-email@gmail.com>
2026-02-12 10:36:08 +0530

    Update

---
+++ COLLEGE/Theory class test of loops/Q1.py
@@ -0,0 +1,4 @@
+x=int(input("input Number : "))
+
+for i in range(1,x+1):
+    print(f"Current no. is : {i} and Cube is :{i**3}")
---
+++ COLLEGE/Theory class test of loops/Q3.py
@@ -0,0 +1,8 @@
+for i in range(1,8):
+    if(i==1 or i==7):
+        for j in range(1,5):
+            print("*",end="")
+        print()
+    else:
+        print("*   *")
+        # print()
